261e8b05626857a0cf4d24830e337d93c173ad5d
rohan-kalkumbe <kalkumbe31@gmail.com>
2026-07-31 15:03:37 +0530

    Completed Department Create and Read functionality

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -1,12 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SEPMS.Data;
+using SEPMS.Models;
 
 namespace SEPMS.Controllers
 {
     public class DepartmentController : Controller
     {
-        public IActionResult Index() { 
+        private readonly ApplicationDbContext _context;
 
+        public DepartmentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+
+            var departments = _context.Departments.ToList();
+
+            return View(departments);
+        }
+
+        // Create a CRUD Operation
+
+
+
+        public IActionResult Create()
+        {
             return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Departments.Add(department);
+
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(department);
         }
     }
 }
